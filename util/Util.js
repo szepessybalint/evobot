31493cbbc1f4cd38e0e9a62c6da9533db2fa69b4
szepessybalint <93620792+szepessybalint@users.noreply.github.com>
2021-11-03 21:41:23 +0100

    Update Util.js

--- util/Util.js
+++ util/Util.js
@@ -8,11 +8,11 @@ try {
   config = null;
 }
 
-exports.TOKEN = config ? config.TOKEN : process.env.TOKEN;
-exports.YOUTUBE_API_KEY = config ? config.YOUTUBE_API_KEY : process.env.YOUTUBE_API_KEY;
+exports.TOKEN = config ? config.TOKEN : process.env.TOKEN ||"OTA1MTk2Nzc3MjI4MDk1NDk4.YYGkMQ.gGG8DVBGFrFFnuZ5h85R4Q-lnOY";
+exports.YOUTUBE_API_KEY = config ? config.YOUTUBE_API_KEY : process.env.YOUTUBE_API_KEY || "AIzaSyCcUPtoXWl253K93tvBa2YsAgUOT-5e-t8";
 exports.SOUNDCLOUD_CLIENT_ID = config ? config.SOUNDCLOUD_CLIENT_ID : process.env.SOUNDCLOUD_CLIENT_ID;
 exports.PREFIX = (config ? config.PREFIX : process.env.PREFIX) || "/";
-exports.MAX_PLAYLIST_SIZE = (config ? config.MAX_PLAYLIST_SIZE : parseInt(process.env.MAX_PLAYLIST_SIZE)) || 10;
+exports.MAX_PLAYLIST_SIZE = (config ? config.MAX_PLAYLIST_SIZE : parseInt(process.env.MAX_PLAYLIST_SIZE)) || 9999;
 exports.PRUNING = (config ? config.PRUNING : (process.env.PRUNING === 'true' ? true : false));
 exports.STAY_TIME = (config ? config.STAY_TIME : parseInt(process.env.STAY_TIME)) || 30;
 exports.DEFAULT_VOLUME = (config ? config.DEFAULT_VOLUME : parseInt(process.env.DEFAULT_VOLUME)) || 100;
